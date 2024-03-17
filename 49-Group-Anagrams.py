762687aaa9a1381e2b864fdeae5f3b16fc957a38
Sarah Hassan <sarraree5@gmail.com>
2024-03-17 08:09:37 +0200

    [49] [Time Beats: 43.78%] [Memory Beats: 76.28%] - LeetPush

--- 49-Group-Anagrams.py
+++ 49-Group-Anagrams.py
@@ -1,6 +1,17 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+    # defaultdict() dont have to check if key is present in the dict
+    # if not in the {} will set it as a key
+        hashMap = defaultdict(list) 
+        for s in strs:
+            sorted_s = ''.join(sorted(s))
+            hashMap[sorted_s].append(s)
+
+        return hashMap.values()
         
+
+#----------------- Another solution using {} ------------------
+
         hashMap = {}
         for s in strs:
             sorted_s = ''.join(sorted(s))
@@ -9,6 +20,3 @@ class Solution:
                 hashMap[sorted_s].append(s)
             else:
                 hashMap[sorted_s] = [s]
-
-        return [i for i in hashMap.values()]
-        
