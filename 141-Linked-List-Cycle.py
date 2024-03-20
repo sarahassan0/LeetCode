518fe958d1e284319770ba8297f9316d0c299e01
Sarah Hassan <sarraree5@gmail.com>
2024-03-20 07:34:37 +0200

    [141] [Time Beats: 33.54%] [Memory Beats: 85.11%] - LeetPush

---
+++ 141-Linked-List-Cycle.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        # tmp = head
+        # nodes = []
+    
+        # while tmp:
+        #     if tmp in nodes:
+        #         return True
+        #     nodes.append(tmp)
+        #     tmp = tmp.next
+        
+        # return False
+
+#---------------- Solution using slow & fast pointers --------
+
+        slow = head
+        fast = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+            if slow == fast:
+                return True
+
+        return False
