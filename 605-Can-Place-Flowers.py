94d65b9f20f628a94c755daf2cc0d520c1a3fab4
Sarah Hassan <sarraree5@gmail.com>
2024-03-24 03:01:32 +0200

    [605] [Time Beats: 73.35%] [Memory Beats: 71.76%] - LeetPush

--- 605-Can-Place-Flowers.py
+++ 605-Can-Place-Flowers.py
@@ -1,21 +1,20 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
 
-        if len(flowerbed) == 1:
-            if flowerbed[0] == 0:
-                return n <= 1
-            else:
-                return n == 0
+        if len(flowerbed) == 1 and flowerbed[0] == 0:
+            return n <= 1
 
         valid = 0
         for i in range(len(flowerbed)-1):
-            if flowerbed[0] == 0 and flowerbed[1] == 0:
-                flowerbed[0] = 1
-                valid += 1
-                continue
+  
             if flowerbed[i] == flowerbed[i+1] == 0:
 
-                if flowerbed[i-1]== 0:
+                if i == 0:
+                    flowerbed[0] = 1
+                    valid += 1
+                    continue
+                    
+                elif flowerbed[i-1]== 0 :
                     flowerbed[i] = 1
                     valid += 1 
 
@@ -23,5 +22,4 @@ class Solution:
                     flowerbed[i+1] = 1
                     valid += 1
 
-        print(flowerbed)
         return valid >= n
