0cb133b1a8eb4ca7b324672cc89dab4107b89d26
Sarah Hassan <sarraree5@gmail.com>
2024-03-24 02:44:05 +0200

    [605] [Time Beats: 34.62%] [Memory Beats: 80.24%] - LeetPush

--- 605-Can-Place-Flowers.py
+++ 605-Can-Place-Flowers.py
@@ -1,36 +1,26 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        if len(flowerbed) ==1:
+
+        if len(flowerbed) == 1:
             if flowerbed[0] == 0:
                 return n <= 1
             else:
                 return n == 0
-            return True if flowerbed[0] - n <= 1 or flowerbed[0] - n == -1 else  False
 
         valid = 0
         for i in range(len(flowerbed)-1):
-            # dd = sum(flowerbed[i-1:i+2])
-            # print(flowerbed[i-1:i+2])
-            # if dd < 2:
-
-
             if flowerbed[0] == 0 and flowerbed[1] == 0:
                 flowerbed[0] = 1
                 valid += 1
                 continue
-            if flowerbed[i]==flowerbed[i+1] == 0 :
-                if flowerbed[i-1]== 0:
-                    flowerbed[i] = 1
-                # else:
-                #     # print(flowerbed[i],flowerbed[i+1] )
-                #     flowerbed[i] = 1
+            if flowerbed[i]==0:
+                if flowerbed[i+1] == 0 :
+                    if flowerbed[i-1]== 0:
+                        flowerbed[i] = 1
                 
-                    valid += 1 
-                if i+1 == len(flowerbed) - 1 and flowerbed[i] ==0:
-                    # pass
-
-                    flowerbed[i+1] = 1
-                    valid += 1
+                        valid += 1 
+                    elif i+1 == len(flowerbed) - 1 :
+                        flowerbed[i+1] = 1
+                        valid += 1
         print(flowerbed)
-        # print(valid)
         return valid >= n
