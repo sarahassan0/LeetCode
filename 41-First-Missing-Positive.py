eca19771e8a45d79933817dd1bc45f3ca8a7d0fa
Sarah Hassan <sarraree5@gmail.com>
2024-03-26 12:44:09 +0200

    [41] [Time Beats: 12.78%] [Memory Beats: 61.27%] - LeetPush

---
+++ 41-First-Missing-Positive.py
@@ -0,0 +1,10 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        nums = set(sorted(nums))
+
+        for i in range(len(nums)):
+            if i+1 not in nums:
+                return i+1
+
+
+        return i+2
