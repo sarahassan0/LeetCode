4cded3ab2d7415476829e9931384f324cac15323
Sarah Hassan <sarraree5@gmail.com>
2024-04-01 05:26:22 +0200

    [1189] [Time Beats: 91.06%] [Memory Beats: 74.35%] - LeetPush

---
+++ 1189-Maximum-Number-of-Balloons.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        
+        freq = {
+            'b': 0,
+            'a': 0,
+            'l': 0,
+            'o': 0,
+            'n': 0,
+        }
+
+        for c in text:
+            if c in freq:
+                freq[c] += 1
+        print(freq)
+        
+        freq['l'] = freq['l'] // 2
+        freq['o'] =  freq['o'] // 2
+
+        return min(freq.values()) 
+         
+
+
