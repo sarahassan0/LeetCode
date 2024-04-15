d809b351a634b759107be7ef0d0e2cc327af51a3
Sarah Hassan <sarraree5@gmail.com>
2024-04-15 17:32:08 +0200

    [129] [Time Beats: 68.90%] [Memory Beats: 26.23%] - LeetPush

--- 129-Sum-Root-to-Leaf-Numbers.py
+++ 129-Sum-Root-to-Leaf-Numbers.py
@@ -5,16 +5,15 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def sumNumbers(self, root: Optional[TreeNode], path_sum="") -> int:
+    def sumNumbers(self, root: Optional[TreeNode], path_sum=0) -> int:
     
         if not root:
             return 0
 
-        path_sum = path_sum + str(root.val)
-    
+        path_sum = path_sum * 10 + root.val
+
         if not root.left and not root.right:
-            return int(path_sum)
+            return path_sum
 
-
-        return self.sumNumbers(root.left,path_sum) + self.sumNumbers(root.right,path_sum)
+        return self.sumNumbers(root.left, path_sum) + self.sumNumbers(root.right, path_sum)
     
