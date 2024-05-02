d176a077a776c03aece38b7a314df8bb237ae51c
Sarah Hassan <sarraree5@gmail.com>
2024-05-03 01:55:14 +0300

    [796] [Time Beats: 91.73%] [Memory Beats: 36.58%] - LeetPush

---
+++ 796-Rotate-String.py
@@ -0,0 +1,17 @@
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+
+        return len(s) == len(goal) and s in goal + goal
+
+# -------------- Another detailed soulotion --------
+
+        # if len(s) != len(goal): return False
+
+        # for i in range(len(s)):
+        #     if s == goal:
+        #         return True
+
+        #     s = s[1:]+s[0]
+        
+        # return False
+        
