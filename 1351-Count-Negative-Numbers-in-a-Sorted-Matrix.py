8e4cc41691e8a4dfd23fe600aacfe24e11282edb
Sarah Hassan <sarraree5@gmail.com>
2024-09-21 19:42:29 +0300

    [1351] [Time Beats: 56.52%] [Memory Beats: 18.91%] - LeetPush

--- 1351-Count-Negative-Numbers-in-a-Sorted-Matrix.py
+++ 1351-Count-Negative-Numbers-in-a-Sorted-Matrix.py
@@ -5,7 +5,6 @@ class Solution:
 
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                print(i, j)
                 if grid[i][j] < 0:
                     nagatives += 1
 
