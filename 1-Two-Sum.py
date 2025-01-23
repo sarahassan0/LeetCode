2bfc70df54502a51cf9a63025df835bbcdf889d7
Sarah Hassan <sarraree5@gmail.com>
2025-01-23 13:54:08 +0200

    [1] [Time Beats: 100.00%] [Memory Beats: 33.57%] - LeetPush

--- 1-Two-Sum.py
+++ 1-Two-Sum.py
@@ -1,9 +1,20 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        seen = {}
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashMap = {}
+
         for i in range(len(nums)):
-            diff = target - nums[i]
-            if diff in seen:
-                return [seen[diff], i]
-            else:
-                seen[nums[i]] = i
+            comp = target - nums[i]
+            if comp in hashMap:
+                return [i, hashMap[comp]]
+        
+            hashMap[nums[i]] = i
+        return []
+
+################
+        # for i in range(len(nums)):
+        #     comp = target - nums[i]
+        #     if comp in nums[i+1:]:
+        #         return [i, nums.index(comp, i+1)]
+
+        # return []
+        
