89935caca90a0d98da806d9dad8ddad14759356c
Sarah Hassan <sarraree5@gmail.com>
2025-01-28 09:56:21 +0200

    [205] [Time Beats: 28.98%] [Memory Beats: 71.86%] - LeetPush

--- 205-Isomorphic-Strings.py
+++ 205-Isomorphic-Strings.py
@@ -1,20 +1,14 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        
-        if len(s) != len(t):
+
+        if len(set(s)) != len(set((t))):
             return False
 
-        pattern = {}
-
+        charHash = {} 
         for i in range(len(s)):
-            
-            if s[i] in pattern and pattern[s[i]] != t[i] :
+            char = s[i]
+            if char in charHash and  charHash[char] != t[i]:
                 return False
-            if s[i] not in pattern and t[i] in pattern.values():
-                return False
-
-            pattern[s[i]] = t[i]
-
+            charHash[char] = t[i]
+     
         return True
-
-        
