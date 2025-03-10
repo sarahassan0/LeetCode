ac94e77086ad1ceef652ffedc8f3807d58947f7b
Sarah Hassan <sarraree5@gmail.com>
2025-03-11 00:41:10 +0200

    [242] [Time Beats: 54.04%] [Memory Beats: 33.33%] - LeetPush

---
+++ Valid-Anagram.py
@@ -0,0 +1,43 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        count = [0] * 26 
+        for i in range(len(s)):
+            count[ord(s[i]) - ord('a')] += 1
+            count[ord(t[i]) - ord('a')] -= 1
+        
+        for i in count:
+            if i != 0:
+                return False
+        return True
+#---------------------------- Another Solutions -----------
+
+        # set_S, set_T = {}, {}
+        
+        # for i in range(len(s)):
+            
+        #     set_S[s[i]] = 1 + set_S.get(s[i], 0)
+        #     set_T[t[i]] = 1 + set_T.get(t[i], 0)
+
+
+        # return set_S == set_T
+    
+#---------------------------- Another Solutions -----------
+
+        # set_S, set_T = defaultdict(int), defaultdict(int)
+        
+        # for i in range(len(s)):
+            
+        #     set_S[i] += 1
+        #     set_T[i] += 1
+
+        # return set_S == set_T
+        
+#---------------------------- Another Solutions -----------
+
+        # return sorted(s) == sorted(t)
+#-----------------------------------------------------
+        # return Counter(s) == Counter(t)
+        
