7ea072291f9204d5db50d8d9d9f764613bba9407
Sarah Hassan <sarraree5@gmail.com>
2025-03-11 20:47:17 +0200

    [1] [Time Beats: 37.45%] [Memory Beats: 6.44%] - LeetPush

--- Two-Sum.py
+++ Two-Sum.py
@@ -1,10 +1,40 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        n = len(nums)
 
-        for i in range(n-1):
-            for j in range(i+1, n):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+#------------------- O(n log n) time, O(n) space -----
 
-        return [] # No solution found
+        indices = []
+
+        for i, n in enumerate(nums):
+            indices.append([n, i])
+        indices.sort()
+
+        r, l = 0, len(nums) - 1
+
+        while r < l:
+            summ = indices[r][0] + indices[l][0]
+            if summ == target:
+                return [indices[r][1], indices[l][1]]
+            elif summ > target:
+                l -= 1
+            else:
+                r += 1
+        return []
+#------------------- O(n) time, O(n) space -----
+        # diffs = {}
+        
+        # for i, n in enumerate(nums):
+        #     diff = target - n
+        #     if diff in diffs:
+        #         return [diffs[diff], i]
+        #     diffs[n] = i
+
+
+# ------------------- O(n2) time ------------------ 
+        # n = len(nums)
+
+        # for i in range(n-1):
+        #     for j in range(i+1, n):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
+
