3a80bf340668025a46de6b052be82b53ee070bda
Sarah Hassan <sarraree5@gmail.com>
2025-03-29 23:41:58 +0200

    [27] [Time Beats: 100.00%] [Memory Beats: 83.38%] - LeetPush

---
+++ Remove-Element.py
@@ -0,0 +1,32 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        idx = 0
+
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[idx] = nums[i]
+                idx += 1
+
+        return idx
+    #-------------------------------
+
+        # i, n = 0, len(nums) 
+
+        # while i < n:
+        #     if nums[i] == val:
+        #         n -= 1
+        #         nums[i] = nums[n]
+        #     else:
+        #         i += 1
+        # return n
+#-----------------------------------------------------
+
+        # tmp = []
+
+        # for n in nums:
+        #     if n != val:
+        #         tmp.append(n)
+        # for i in range(len(tmp)):
+        #     nums[i] = tmp[i]
+
+        # return len(tmp)
