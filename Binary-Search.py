332b7c2d71981c0fe125cebea1b6519973007e80
Sarah Hassan <sarraree5@gmail.com>
2025-03-29 16:07:40 +0200

    [704] [Time Beats: 100.00%] [Memory Beats: 44.90%] - LeetPush

--- Binary-Search.py
+++ Binary-Search.py
@@ -14,3 +14,21 @@ class Solution:
                 high = mid - 1
             
         return -1
+
+#-------------------- Recursive solution, time O(logn), space O(logn)-------
+# class Solution:
+#     def search(self, nums: List[int], target: int) -> int:
+#         return self.binarySearch(0, len(nums) - 1, nums, target)
+
+#     def binarySearch(self, low, high, nums, target):
+#         if low > high:
+#             return -1 
+#         mid = low + ((high - low) // 2)
+        
+#         if  target == nums[mid]:
+#             return mid
+#         elif target > nums[mid]:
+#             return self.binarySearch(mid + 1, high, nums, target)
+#         else:
+#             return  self.binarySearch(low, mid - 1, nums, target)
+
