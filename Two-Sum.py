08ded584035719db016a3b339a6be9943370c1bf
Sarah Hassan <sarraree5@gmail.com>
2025-04-20 14:58:25 +0200

    [1] [Time Beats: 50.47%] [Memory Beats: 6.36%] - LeetPush

--- Two-Sum.py
+++ Two-Sum.py
@@ -3,23 +3,20 @@ class Solution:
 
 #------------------- O(n log n) time, O(n) space -----
 
-        indices = []
-
-        for i, n in enumerate(nums):
-            indices.append([n, i])
+        indices = [(n, i) for i, n in enumerate(nums)]
         indices.sort()
 
-        r, l = 0, len(nums) - 1
-
-        while r < l:
-            summ = indices[r][0] + indices[l][0]
+        l, r = 0, len(nums) - 1
+        while l < r:
+            summ =  indices[r][0] + indices[l][0]
             if summ == target:
                 return [indices[r][1], indices[l][1]]
             elif summ > target:
-                l -= 1
-            else:
-                r += 1
+                r -= 1
+            else: 
+                l += 1
         return []
+
 #------------------- O(n) time, O(n) space -----
         # diffs = {}
         
@@ -37,4 +34,3 @@ class Solution:
         #     for j in range(i+1, n):
         #         if nums[i] + nums[j] == target:
         #             return [i, j]
-
