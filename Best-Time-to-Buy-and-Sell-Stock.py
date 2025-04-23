5bcb5f0247372d58eb7c2de3081babe87ea3869f
Sarah Hassan <sarraree5@gmail.com>
2025-04-23 14:28:20 +0200

    [121] [Time Beats: 74.41%] [Memory Beats: 33.19%] - LeetPush

---
+++ Best-Time-to-Buy-and-Sell-Stock.py
@@ -0,0 +1,40 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        less_price, max_profit = prices[0], 0
+
+        for today_price in prices:
+            if today_price < less_price:
+                less_price = today_price
+            else:
+                today_profit = today_price - less_price
+                max_profit = max(max_profit , today_profit)
+
+        return max_profit
+# ---------------- Solution using two pointers technique ----
+
+        # l, r = 0, 1
+        # max_profit = 0
+
+        # while r < len(prices):
+        #     if prices[r] > prices[l]:
+        #         profit = prices[r] - prices[l]
+        #         max_profit = max(profit, max_profit)
+        #     else:
+        #         l = r
+        #     r += 1
+        # return max_profit
+
+#-------------------- Time limit in case 200 -----------
+
+        # maxi = 0
+        
+        # for i in range(1, len(prices)):
+        #     profit =  max(prices[i:]) - prices[i-1]
+        #     maxi = max(profit, maxi)
+        
+
+        # return 0 if maxi <= 0 else maxi
+
+
+        
