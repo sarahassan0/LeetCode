1a1b25a6227f04b1f9b305bef9f80a28ecf4d23e
Sarah Hassan <sarraree5@gmail.com>
2025-05-23 16:32:20 +0300

    [3024] [Time Beats: 100.00%] [Memory Beats: 82.75%] - LeetPush

---
+++ Type-of-Triangle.py
@@ -0,0 +1,14 @@
+class Solution:
+    def triangleType(self, nums: List[int]) -> str:
+
+        a, b, c = nums
+        if a + b <= c or a + c <= b or b + c <= a:
+            return 'none'
+
+        if a == b == c:
+            return 'equilateral'
+        if a == b or a == c or b == c:
+            return 'isosceles'
+        
+        return 'scalene'
+            
