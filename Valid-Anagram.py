86032076920bd51bca3332d0dbd0be97d11559ab
Sarah Hassan <sarraree5@gmail.com>
2025-07-15 22:33:02 +0300

    [242] [Time Beats: 72.23%] [Memory Beats: 97.53%] - LeetPush

--- Valid-Anagram.py
+++ Valid-Anagram.py
@@ -1,6 +1,6 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-      
+
         if len(s) != len(t): return False
 
         count = [0] * 26
@@ -12,8 +12,7 @@ class Solution:
             if n != 0:
                 return False
         return True
-
-    
+   
 #---------------------------- Another Solutions -----------
 
         # set_S, set_T = {}, {}
