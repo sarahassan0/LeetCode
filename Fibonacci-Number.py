015de6e65a2b4a855e0bd6acbc9ea6bff9f1dc5a
Sarah Hassan <sarraree5@gmail.com>
2025-07-22 19:32:21 +0300

    [509] [Time Beats: 32.77%] [Memory Beats: 23.22%] - LeetPush

---
+++ Fibonacci-Number.py
@@ -0,0 +1,26 @@
+class Solution:
+    def fib(self, n: int) -> int:
+
+################### Iterative - O(n) ##################
+
+
+        # if n == 0: return 0
+        # if n <= 2: return 1
+
+        # arr = [0, 1]
+
+        # for i in range(2, n+1):
+        #     arr.append(arr[i-1] + arr[i-2])
+
+        # return arr[n]
+
+################### Recursion - O(2^n) ##################
+
+        if n == 0: return 0
+        if n <= 2: return 1
+
+        return self.fib(n-1) + self.fib(n-2)
+
+        
+        
+        
