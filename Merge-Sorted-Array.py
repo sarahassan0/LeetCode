2e54d7c039c7619d8bb565e820e1313b4b429695
Sarah Hassan <sarraree5@gmail.com>
2025-09-07 10:15:14 +0300

    [88] [Time Beats: 100.00%] [Memory Beats: 71.46%] - LeetPush

--- Merge-Sorted-Array.py
+++ Merge-Sorted-Array.py
@@ -3,26 +3,24 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        a, b, write_index = m-1, n-1, m + n - 1
+        a, b, writing_index = m-1, n-1, m+n-1
 
         while b >= 0:
-            if a >= 0 and nums1[a] > nums2[b]:
-                nums1[write_index] = nums1[a]
-                a -= 1
+            if a >= 0 and nums2[b] < nums1[a]:   
+                nums1[writing_index] = nums1[a]
+                a -= 1 
             else:
-                nums1[write_index] = nums2[b]
+                nums1[writing_index] = nums2[b]
                 b -= 1
 
-            write_index -= 1
+            writing_index -= 1
 
-    
+
     #-------------------------------
         # for i in range(n):
         #     nums1[m+i] = nums2[i]
 
         # nums1.sort()
-
-
 
 
 
