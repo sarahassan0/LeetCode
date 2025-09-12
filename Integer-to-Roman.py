e9101d5ddb7676906be4b34898ecb4469ddadc43
Sarah Hassan <sarraree5@gmail.com>
2025-09-12 23:55:09 +0300

    [12] [Time Beats: 52.73%] [Memory Beats: 24.90%] - LeetPush

---
+++ Integer-to-Roman.py
@@ -0,0 +1,19 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+
+        nums_map = {
+            1: "I",
+            5: "V",    4: "IV",
+            10: "X",   9: "IX",
+            50: "L",   40: "XL",
+            100: "C",  90: "XC",
+            500: "D",  400: "CD",
+            1000: "M", 900: "CM",
+        }
+
+        roman = ""
+        for n in sorted(nums_map.keys(), reverse=True):
+            if num // n >= 1:
+                roman += nums_map[n] * (num // n)
+                num -= n * (num // n)
+        return roman
