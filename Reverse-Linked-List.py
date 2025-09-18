214fcd66a92fad03c2dbfa385cc470824e51d012
Sarah Hassan <sarraree5@gmail.com>
2025-09-18 14:28:05 +0300

    [206] [Time Beats: 100.00%] [Memory Beats: 14.29%] - LeetPush

--- Reverse-Linked-List.py
+++ Reverse-Linked-List.py
@@ -4,36 +4,13 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-#----------------Two pointers solution -------
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:\
 
-        prev, curr = None, head
-
+    #------------- iterative solution ------------------
+        curr, prev = head, None
         while curr:
             tmp = curr.next
             curr.next = prev
             prev, curr = curr, tmp
-
         return prev
-
-
-#---------------- Stack solution --------------
-        # stack = []
-        # cur = head
-
-        # while cur:
-        #     stack.append(cur)
-        #     cur = cur.next
-
-        # head = stack.pop() if stack else None
-        # cur = head
-
-        # while stack:
-        #     cur.next = stack.pop()
-        #     cur = cur.next
-
-        # if cur:
-        #     cur.next = None
-
-        # return head
-        
+    #------------- recursive solution ------------------
