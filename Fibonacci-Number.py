b1a9655de6cc922b384232b8a31cb3f48e1f75eb
Sarah Hassan <sarraree5@gmail.com>
2025-09-18 15:12:39 +0300

    [509] [Time Beats: 71.95%] [Memory Beats: 63.89%] - LeetPush

--- Fibonacci-Number.py
+++ Fibonacci-Number.py
@@ -3,23 +3,18 @@ class Solution:
 
 ################### Iterative - O(n) ##################
 
+        arr = [0, 1]
 
-        # if n == 0: return 0
-        # if n <= 2: return 1
+        for i in range(2, n+1):
+            arr.append(arr[i-1] + arr[i-2])
 
-        # arr = [0, 1]
-
-        # for i in range(2, n+1):
-        #     arr.append(arr[i-1] + arr[i-2])
-
-        # return arr[n]
+        return arr[n]
 
 ################### Recursion - O(2^n) ##################
 
-        if n == 0: return 0
-        if n <= 2: return 1
-
-        return self.fib(n-1) + self.fib(n-2)
+    #    if n == 0: return 0
+    #    if n <= 2: return 1
+    #    return self.fib(n-1) + self.fib(n-2)
 
         
         
