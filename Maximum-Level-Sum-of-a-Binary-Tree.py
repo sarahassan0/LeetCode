8c229b2f1e0f70fb792d1a78bfc674c4e6e834bd
Sarah Hassan <sarraree5@gmail.com>
2025-09-22 14:30:50 +0300

    [1161] [Time Beats: 64.97%] [Memory Beats: 44.32%] - LeetPush

--- Maximum-Level-Sum-of-a-Binary-Tree.py
+++ Maximum-Level-Sum-of-a-Binary-Tree.py
@@ -6,35 +6,29 @@
 #         self.right = right
 class Solution:
     def maxLevelSum(self, root: Optional[TreeNode]) -> int:
-        if root is None:
+        if not root:
             return 0
-        queue = deque([root])
 
-        maxi = [float(-inf), 0]
+        queue = deque([root])
+        maxi = [float('-inf'), 0]
         level = 0
+
         while queue:
             level_size = len(queue)
             level_sum = 0
-            level += 1
+            level += 1 
 
             for _ in range(level_size):
                 curr = queue.popleft()
-                level_sum += curr.val 
+                level_sum += curr.val
 
                 if curr.left:
                     queue.append(curr.left)
                 if curr.right:
                     queue.append(curr.right)
-  
+
             if level_sum > maxi[0]:
                 maxi = [level_sum, level]
                 
         return maxi[1]
-            
-
-
-            
-
-        
-
-        
+    
