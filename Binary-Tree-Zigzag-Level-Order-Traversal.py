4dad0f867054ec6c5793ef5fc9627a4ba3015f1c
Sarah Hassan <sarraree5@gmail.com>
2025-09-23 17:11:12 +0300

    [103] [Time Beats: 100.00%] [Memory Beats: 95.46%] - LeetPush

--- Binary-Tree-Zigzag-Level-Order-Traversal.py
+++ Binary-Tree-Zigzag-Level-Order-Traversal.py
@@ -8,57 +8,57 @@ from collections import deque
 class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
 
-        if root is None:
+        if not root:
             return []
+        
         queue = deque([root])
         result = []
         order = -1
 
         while queue:
-            level_size = len(queue) 
-            order *= -1 
-            level = []
-            
+            order *= -1
+            level_size = len(queue)
+            curr_level = []
+
             for _ in range(level_size):
                 curr = queue.popleft()
-                level.append(curr.val)
-
+                curr_level.append(curr.val)
+            
                 if curr.left:
                     queue.append(curr.left)
                 if curr.right:
                     queue.append(curr.right)
-            result.append(level[::order])
+            result.append(curr_level[::order])
 
         return result
 
-
 #---- Using Queue, more effeciant than level[::order] because it copy and rewrite the arr --------
 
-        if root is None:
+        if not root:
             return []
+        
         queue = deque([root])
         result = []
         order = -1
 
         while queue:
-            level_size = len(queue) 
-            order *= -1 
-            level = deque()
-            
+            order *= -1
+            level_size = len(queue)
+            curr_level = deque()
+
             for _ in range(level_size):
                 curr = queue.popleft()
 
                 if order == 1:
-                    level.append(curr.val)
+                    curr_level.append(curr.val)
                 else:
-                    level.appendleft(curr.val)
-
+                    curr_level.appendleft(curr.val)
+            
                 if curr.left:
                     queue.append(curr.left)
                 if curr.right:
                     queue.append(curr.right)
-
-            result.append(list(level))
+            result.append(list(curr_level))
+            
         return result
-        
         
